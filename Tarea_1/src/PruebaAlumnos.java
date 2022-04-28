e9109022a790aa11d3a653240a04fe6adec80f13
Laura <lauramunoz.daw@ciudadescolarfp.es>
2022-04-28 21:39:06 +0200

    "Commit despues de Merge"

--- Tarea_1/src/PruebaAlumnos.java
+++ Tarea_1/src/PruebaAlumnos.java
@@ -4,7 +4,11 @@ public class PruebaAlumnos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Listado de alumnos 1º DAW");
+		System.out.println("Alumno 1:");
 		
+		Alumno alumno1=new Alumno("Laura","25","1234567");
+		
+		System.out.println(alumno1);
 
 	}
 
